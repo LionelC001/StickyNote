e09abea6009c0b47682550ef513a8626afff4cfc
LionelC001 <lionelc15001@gmail.com>
2018-05-02 23:47:45 +0800

    version10 :
    - can choose and reset wallpaper now
    - can crop photo for wallpaper now
    
    lack:
    - maybe sharing

--- app/src/main/java/com/lionel/stickynote/MainActivity.java
+++ app/src/main/java/com/lionel/stickynote/MainActivity.java
@@ -1,18 +1,32 @@
 package com.lionel.stickynote;
 
+import android.Manifest;
 import android.annotation.SuppressLint;
 import android.app.ActivityOptions;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
 import android.support.constraint.ConstraintLayout;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.transition.Transition;
 import android.transition.TransitionInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewTreeObserver;
 import android.view.Window;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
@@ -20,6 +34,8 @@ import com.lionel.stickynote.customview.Paper;
 import com.lionel.stickynote.fieldclass.PaperProperty;
 import com.lionel.stickynote.sqliteopenhelper.PaperContentDbHelper;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.lang.reflect.Type;
 import java.util.ArrayList;
 
@@ -27,6 +43,11 @@ import static com.lionel.stickynote.PaperContentActivity.DB_NAME;
 
 
 public class MainActivity extends AppCompatActivity implements Paper.DeletePaperInterface, Paper.OpenPaperContent {
+    private static final int REQUEST_CODE_FOR_OPEN_CONTENT = 100;
+    private static final int REQUEST_CODE_FOR_PICK_PHOTO_ = 200;
+    private static final int REQUEST_CODE_FOR_CROP_PHOTO = 300;
+    private static final int REQUEST_PERMISSION_FOR_WRITE_EXTERNAL_STORAGE = 999;
+
     private ConstraintLayout mRootView;
     private ArrayList<PaperProperty> mPaperPropertyArrayList; // store Paper object
     private int mChildViewCount, mPaperId;
@@ -47,6 +68,141 @@ public class MainActivity extends AppCompatActivity implements Paper.DeletePaper
         setupDeleteRegion();
         setupTransition();
         showIntroPages();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        // only use setupDeskTop() in onStart() in the following situations: first time to startup app or the paper was disappeared
+        if (mChildViewCount == 2 || mRootView.getChildCount() != mChildViewCount) {
+            setupDeskTop();
+            mChildViewCount = mRootView.getChildCount();
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        // save Paper Property to sharedPreferences
+        SharedPreferences.Editor sharedPreferences =
+                getSharedPreferences("PaperPropertyData", MODE_PRIVATE).edit();
+        Gson gson = new Gson();
+        String json = gson.toJson(mPaperPropertyArrayList);
+        sharedPreferences.putString("PaperProperty", json);
+
+        // to save Paper Id is how much to add now
+        sharedPreferences.putInt("PaperId", mPaperId);
+        sharedPreferences.apply();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menuItemBackground:
+                if (ActivityCompat.checkSelfPermission(this,
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE) !=
+                        PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(this,
+                            new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                            REQUEST_PERMISSION_FOR_WRITE_EXTERNAL_STORAGE);
+                } else {
+                    pickPhoto();
+                }
+                break;
+            case R.id.menuItemResetBackground:
+                mRootView.setBackgroundColor(Color.parseColor("#ffffff"));
+                SharedPreferences.Editor spBackgroundPic = getSharedPreferences("BackgroundData", MODE_PRIVATE).edit();
+                spBackgroundPic.remove("PicUri");
+                spBackgroundPic.apply();
+                break;
+            case R.id.menuItemIntroPage:
+                startActivity(new Intent(this, IntroPageActivity.class));
+                break;
+            case R.id.menuItemAbout:
+
+                break;
+        }
+        return true;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode) {
+            case REQUEST_CODE_FOR_PICK_PHOTO_:
+                if (data != null) cropPhoto(data.getData());
+                break;
+            case REQUEST_CODE_FOR_CROP_PHOTO:
+                if (data != null) {
+                    Uri cropUri = data.getData();
+                    if (cropUri != null) {
+                        Intent it = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE, cropUri);
+                        sendBroadcast(it);
+                        try {
+                            Bitmap cropBitmap = BitmapFactory.decodeStream(getContentResolver().openInputStream(cropUri), null, null);
+                            mRootView.setBackground(new BitmapDrawable(getResources(), cropBitmap));
+                        } catch (FileNotFoundException e) {
+                            e.printStackTrace();
+                            Toast.makeText(this, "Can not find file", Toast.LENGTH_LONG).show();
+                        }
+                        // to save background picture which user chose
+                        SharedPreferences.Editor spBackgroundPic = getSharedPreferences("BackgroundData", MODE_PRIVATE).edit();
+                        spBackgroundPic.putString("PicUri", cropUri.toString());
+                        spBackgroundPic.apply();
+                    }
+                }
+                break;
+            default:
+                isReenter = true;
+                break;
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == REQUEST_PERMISSION_FOR_WRITE_EXTERNAL_STORAGE) {
+            if (grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this, "Need Permission for write_external_storage", Toast.LENGTH_LONG).show();
+            } else {
+                pickPhoto();
+            }
+        }
+    }
+
+    private void pickPhoto() {
+        Intent getIntent = new Intent(Intent.ACTION_GET_CONTENT);
+        Intent pickIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        getIntent.setType("image/*");
+        pickIntent.setType("image/*");
+        Intent chooserIntent = Intent.createChooser(getIntent, "Select Wallpaper");
+        chooserIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS, new Intent[]{pickIntent});
+        startActivityForResult(chooserIntent, REQUEST_CODE_FOR_PICK_PHOTO_);
+    }
+
+    private void cropPhoto(Uri uri) {
+        String tempFileName = "StickyNote_" + System.currentTimeMillis() + ".jpg";
+        File cropFile = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES), tempFileName);
+        Uri cropUri = Uri.fromFile(cropFile);
+
+        Intent cropIntent = new Intent("com.android.camera.action.CROP");
+        cropIntent.setDataAndType(uri, "image/*");
+        cropIntent.putExtra("crop", "true");
+        cropIntent.putExtra("aspectX", 1);
+        cropIntent.putExtra("aspectY", 1.56);
+        cropIntent.putExtra("outputX", 1080);
+        cropIntent.putExtra("outputY", 1680);
+        cropIntent.putExtra("scale", "true");
+        cropIntent.putExtra("outputFormat", Bitmap.CompressFormat.JPEG.toString());
+        cropIntent.putExtra("return-data", false);
+        cropIntent.putExtra(MediaStore.EXTRA_OUTPUT, cropUri);
+        startActivityForResult(cropIntent, REQUEST_CODE_FOR_CROP_PHOTO);
     }
 
     private void showIntroPages() {
@@ -76,20 +232,19 @@ public class MainActivity extends AppCompatActivity implements Paper.DeletePaper
         Transition transition2 = TransitionInflater.from(this).inflateTransition(R.transition.transition2);
         getWindow().setExitTransition(transition1);
         getWindow().setReenterTransition(transition2);
-        getWindow().setAllowEnterTransitionOverlap(true);
     }
 
     @Override
     public void onEnterAnimationComplete() {
         // exit animation would call this method, too. have to prevent it.
         if (isReenter) {
-            setupPapers();
+            setupDeskTop();
             isReenter = false;
         }
         super.onEnterAnimationComplete();
     }
 
-    private void setupPapers() {
+    private void setupDeskTop() {
         // to avoid there is view and data remain
         mRootView.removeViews(2, mRootView.getChildCount() - 2);
         mPaperPropertyArrayList.clear();
@@ -112,6 +267,24 @@ public class MainActivity extends AppCompatActivity implements Paper.DeletePaper
             Paper paper = new Paper(this, pp);
             mRootView.addView(paper);
         }
+
+        // setBackground if user have chosen picture before.
+        SharedPreferences sharedPreferencesBackground = getSharedPreferences("BackgroundData", MODE_PRIVATE);
+        String backgroundUri = sharedPreferencesBackground.getString("PicUri", null);
+        if (backgroundUri != null) {
+            Uri cropUri = Uri.parse(backgroundUri);
+            try {
+                Bitmap cropBitmap = BitmapFactory.decodeStream(getContentResolver().openInputStream(cropUri), null, null);
+                mRootView.setBackground(new BitmapDrawable(getResources(), cropBitmap));
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+                Toast.makeText(this, "Can not find wallpaper file", Toast.LENGTH_LONG).show();
+            }
+        } else {
+            mRootView.setBackgroundColor(Color.parseColor("#ffffff"));
+        }
+        // to avoid content transition preforms incorrect
+        mRootView.setTransitionGroup(false);
     }
 
     public void AddPaper(View view) {
@@ -151,37 +324,6 @@ public class MainActivity extends AppCompatActivity implements Paper.DeletePaper
         bundle.putSerializable("PaperProperty", pp);
         bundle.putSerializable("PaperPropertyList", mPaperPropertyArrayList);
         intent.putExtras(bundle);
-        startActivityForResult(intent, 100, ActivityOptions.makeSceneTransitionAnimation(this).toBundle());
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        // only use setupPapers() in onStart() in the following situations: first time to startup app or the paper was disappeared
-        if (mChildViewCount == 2 || mRootView.getChildCount() != mChildViewCount) {
-            setupPapers();
-            mChildViewCount = mRootView.getChildCount();
-        }
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        // save Paper Property to sharedPreferences
-        SharedPreferences.Editor sharedPreferences =
-                getSharedPreferences("PaperPropertyData", MODE_PRIVATE).edit();
-        Gson gson = new Gson();
-        String json = gson.toJson(mPaperPropertyArrayList);
-        sharedPreferences.putString("PaperProperty", json);
-
-        // to save Paper Id is how much to add now
-        sharedPreferences.putInt("PaperId", mPaperId);
-        sharedPreferences.apply();
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        isReenter = true;
-        super.onActivityResult(requestCode, resultCode, data);
+        startActivityForResult(intent, REQUEST_CODE_FOR_OPEN_CONTENT, ActivityOptions.makeSceneTransitionAnimation(this).toBundle());
     }
 }
--- app/src/main/java/com/lionel/stickynote/PaperContentActivity.java
+++ app/src/main/java/com/lionel/stickynote/PaperContentActivity.java
@@ -52,12 +52,12 @@ public class PaperContentActivity extends AppCompatActivity {
 
     // Color for StatusBar, RootView, Title, FloatingActionButtonChild, ItemBG, ItemIndex, ItemText
     private String[][] mColorTheme = {
-            {"#FF5C5B5B", "#AF626262", "#FF000000", "#00FFFFFF", "#AFFFFFFF", "#FF000000", "#FF000000"},
-            {"#DABA236D", "#AFefb2d0", "#DA720038", "#FF9B6C83", "#AF00ced1", "#DAA10050", "#DA620131"},
-            {"#FF99C400", "#D6B1D434", "#D63F5000", "#C979A135", "#AFFCD517", "#FF5C7500", "#D63F5000"},
-            {"#FF8F6D8F", "#E08B468B", "#FF000000", "#FFFFA763", "#AFFE7D1A", "#FF000000", "#FF000000"},
-            {"#FFCE6D1B", "#AFFF7500", "#F47B3E0B", "#E9E0C26F", "#AFfcd0ab", "#FF763702", "#FF763702"},
-            {"#FF002D92", "#DC0450FB", "#FF002965", "#EEFF6D6D", "#AFFF3535", "#FF002965", "#FF002965"}};
+            {"#FF5C5B5B", "#E1626262", "#FF000000", "#00FFFFFF", "#AFFFFFFF", "#FF000000", "#FF000000"},
+            {"#DABA236D", "#E1efb2d0", "#DA720038", "#FF9B6C83", "#AF00ced1", "#DAA10050", "#DA620131"},
+            {"#FF99C400", "#E1B1D434", "#D63F5000", "#C979A135", "#AFFCD517", "#FF5C7500", "#D63F5000"},
+            {"#FF8F6D8F", "#E18B468B", "#FF000000", "#FFFFA763", "#AFFE7D1A", "#FF000000", "#FF000000"},
+            {"#FFCE6D1B", "#E1FF7500", "#F47B3E0B", "#E9E0C26F", "#AFfcd0ab", "#FF763702", "#FF763702"},
+            {"#FF002D92", "#E10450FB", "#FF002965", "#EEFF6D6D", "#AFFF3535", "#FF002965", "#FF002965"}};
 
     // allow ColorPickerBlock to call this activity to make some color set
     private static class ColorPickerHandler extends Handler {
